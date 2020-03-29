1d56c24574d2ac78621a1892728e54d065d5a77f
Andria-Hibe <andriacohibe@gmail.com>
2020-03-29 23:28:34 +1300

    Parse event body from urlencoded to object

--- handler.js
+++ handler.js
@@ -1,14 +1,87 @@
-"use strict";
-const { tagEvent } = require("./serverless_sdk");
+"use strict"
 
-module.exports.hello = async event => {
-  tagEvent("custom-tag", "hello world", { custom: { tag: "data" } });
+const fetch = require('node-fetch')
 
-  return {
+const GITHUB_URL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Easy'
+
+function groupIssuesIntoMessages (array, size) {
+  const blocksArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    const lastBlock = blocksArray[blocksArray.length - 1]
+    
+    if (!lastBlock || lastBlock.length === size) {
+      blocksArray.push([array[i]])
+    } else {
+      lastBlock.push(array[i])
+    }
+  }
+  return blocksArray.map(block => {
+    return {
+      blocks: block
+    }
+  })
+}
+
+function parseBody (body) {
+  const bodyArray = body.split('&')
+
+  const bodyObject = bodyArray.reduce(
+    (accumulator, currentValue) => {
+      const keyValue = currentValue.split('=')
+
+      accumulator[keyValue[0]] = keyValue[1]
+
+      return accumulator
+    }, {}
+  )
+  return bodyObject
+}
+
+module.exports.github = async event => {
+  const body = parseBody(event.body)
+
+  const githubResponse = await fetch(GITHUB_URL)
+    console.log(`github response: ${githubResponse}`)
+  const githubJSON = await githubResponse.json()
+    console.log(`github JSON: ${githubJSON}`)
+
+  const issuesArray = githubJSON.reduce(
+    (accumulator, issue) => {
+      const issueURL = 'https://' + issue.url.slice(12, 23) + issue.url.slice(29)
+
+      accumulator.push(
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `#${issue.number} - <${issueURL}|*${issue.title}*>`
+          }
+        },
+        {
+          type: "section",
+          fields: issue.labels.map(label => {
+            return {
+              type: "plain_text",
+              text: label.name
+            }
+          })
+        },
+        {
+          type: "divider"
+        }
+      )
+      return accumulator
+    }, []
+  )
+
+  console.log(`issues array after reduce: ${(groupIssuesIntoMessages(issuesArray, 45))}`)
+
+  return { //example
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
     },
     body: JSON.stringify(
       {
@@ -19,7 +92,4 @@ module.exports.hello = async event => {
       2
     )
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
