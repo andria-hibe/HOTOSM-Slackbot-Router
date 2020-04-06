1615f28b6262b904f7d71429ac65f0157b0fe9c8
Andria-Hibe <andriacohibe@gmail.com>
2020-04-07 03:51:55 +1200

    See if HOT TM is currently operational

---
+++ healthCheck.js
@@ -0,0 +1,48 @@
+"use strict"
+
+const fetch = require('node-fetch')
+
+const TM_URL = 'https://tasks.hotosm.org/api/health-check'
+
+const slackBlock = (status) => {
+  if (status === 'healthy') {
+    return {
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: ":white_check_mark: The Tasking Manager is currently operational"
+			}
+		}
+  } else {
+    return {
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: ":heavy_exclamation_mark: The Tasking Manager cannot be reached right now"
+			}
+		}
+  }
+}
+
+module.exports.healthCheck = async event => {
+  const taskingManagerResponse = await fetch(TM_URL)
+  const responseJSON = await taskingManagerResponse.json()
+  const status = responseJSON.status
+
+  const slackMessage = {
+    blocks: [
+      slackBlock(status)
+    ]
+  }
+
+  console.log(`message: ${slackMessage}`)
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify(slackMessage)
+  }
+}
