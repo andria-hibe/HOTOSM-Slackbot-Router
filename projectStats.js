2ff704bcb667843193cf50fb541671d69010b054
Andria-Hibe <andriacohibe@gmail.com>
2020-04-07 22:58:20 +1200

    Refactpr projectStats function

--- projectStats.js
+++ projectStats.js
@@ -1,72 +1,59 @@
 'use strict'
 
 const fetch = require('node-fetch')
+const { parseBody } = require('./utils')
 
-function parseBody(body) {
-  const bodyArray = body.split('&')
-
-  const bodyObject = bodyArray.reduce((accumulator, currentValue) => {
-    const keyValue = currentValue.split('=')
-
-    accumulator[keyValue[0]] = keyValue[1]
-
-    return accumulator
-  }, {})
-  return bodyObject
+const createSlackResponse = (blocks) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      blocks: blocks,
+    }),
+  }
 }
 
 module.exports.projectStats = async (event) => {
   const body = parseBody(event.body)
   const projectID = body.text
 
-  if (projectID.match(/^\d+$/)) {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
+  const projectIdHasNonDigit = !!projectID.match(/\D/)
+  if (projectIdHasNonDigit) {
+    const invalidIdBlock = [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: ':x: Invalid project ID',
+        },
       },
-      body: JSON.stringify({
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: ':x: Invalid project ID',
-            },
-          },
-        ],
-      }),
-    }
+    ]
+    return createSlackResponse(invalidIdBlock)
   }
 
   const taskingManagerURL = `https://tasks.hotosm.org/api/v1/project/${projectID}/summary`
-  const projectURL = `https://tasks.hotosm.org/project/${projectID}`
 
   const taskingManagerResponse = await fetch(taskingManagerURL)
   const taskingManagerJSON = await taskingManagerResponse.json()
 
   if (taskingManagerJSON.Error) {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
+    const errorBlock = [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `:x: ${taskingManagerJSON.Error}`,
+        },
       },
-      body: JSON.stringify({
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: `:x: ${taskingManagerJSON.Error}`,
-            },
-          },
-        ],
-      }),
-    }
+    ]
+
+    return createSlackResponse(errorBlock)
   }
 
+  // Change null values in data to 'N/A' instead for readability
   for (const data in taskingManagerJSON) {
     if (taskingManagerJSON[data] === null) {
       taskingManagerJSON[data] = 'N/A'
@@ -86,71 +73,64 @@ module.exports.projectStats = async (event) => {
     status,
   } = taskingManagerJSON
 
-  const slackMessage = {
-    blocks: [
-      {
-        type: 'section',
-        fields: [
-          {
-            type: 'mrkdwn',
-            text: `*Priority*: ${projectPriority}`,
-          },
-          {
-            type: 'mrkdwn',
-            text: `*Level*: ${mapperLevel}`,
-          },
-        ],
-      },
-      {
-        type: 'divider',
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `<${projectURL}|${projectId} - ${name}*>`,
-        },
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `*${status}* - ${shortDescription}`,
-        },
-      },
-      {
-        type: 'divider',
-      },
-      {
-        type: 'section',
-        fields: [
-          {
-            type: 'mrkdwn',
-            text: `*Organisation*: ${organisationTag}`,
-          },
-          {
-            type: 'mrkdwn',
-            text: `*Campaign*: ${campaignTag}`,
-          },
-          {
-            type: 'mrkdwn',
-            text: `*${percentMapped}%* Mapped`,
-          },
-          {
-            type: 'mrkdwn',
-            text: `*${percentValidated}%* Validated`,
-          },
-        ],
-      },
-    ],
-  }
+  const projectURL = `https://tasks.hotosm.org/project/${projectID}`
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+  const successBlock = [
+    {
+      type: 'section',
+      fields: [
+        {
+          type: 'mrkdwn',
+          text: `*Priority*: ${projectPriority}`,
+        },
+        {
+          type: 'mrkdwn',
+          text: `*Level*: ${mapperLevel}`,
+        },
+      ],
     },
-    body: JSON.stringify(slackMessage),
-  }
+    {
+      type: 'divider',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `<${projectURL}|*${projectId} - ${name}*>`,
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*${status}* - ${shortDescription}`,
+      },
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'section',
+      fields: [
+        {
+          type: 'mrkdwn',
+          text: `*Organisation*: ${organisationTag}`,
+        },
+        {
+          type: 'mrkdwn',
+          text: `*Campaign*: ${campaignTag}`,
+        },
+        {
+          type: 'mrkdwn',
+          text: `*${percentMapped}%* Mapped`,
+        },
+        {
+          type: 'mrkdwn',
+          text: `*${percentValidated}%* Validated`,
+        },
+      ],
+    },
+  ]
+
+  return createSlackResponse(successBlock)
 }
