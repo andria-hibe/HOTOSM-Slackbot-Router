0d8d9674136a07950c011bc40a2faf933d3da3fa
Andria-Hibe <andriacohibe@gmail.com>
2020-04-07 20:46:27 +1200

    Refactor code

--- easyIssues.js
+++ easyIssues.js
@@ -1,102 +1,82 @@
-"use strict"
+'use strict'
 
 const fetch = require('node-fetch')
+const { parseBody } = require('./utils')
 
-const GITHUB_URL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Easy'
+const GITHUB_URL =
+  'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Easy'
+const BLOCK_COUNT = 45
 
-function groupIssuesIntoMessages (array, size) {
+function groupIssuesIntoMessages(array, size) {
   const blocksArray = []
 
   for (let i = 0; i < array.length; i++) {
     const lastBlock = blocksArray[blocksArray.length - 1]
-    
+
     if (!lastBlock || lastBlock.length === size) {
       blocksArray.push([array[i]])
     } else {
       lastBlock.push(array[i])
     }
   }
-  return blocksArray.map(block => {
+  return blocksArray.map((block) => {
     return {
-      blocks: block
+      blocks: block,
     }
   })
 }
 
-function parseBody (body) {
-  const bodyArray = body.split('&')
-
-  const bodyObject = bodyArray.reduce(
-    (accumulator, currentValue) => {
-      const keyValue = currentValue.split('=')
-
-      accumulator[keyValue[0]] = keyValue[1]
-
-      return accumulator
-    }, {}
-  )
-  return bodyObject
-}
-
-function parseURL (url) {
-  const colon = /%3A/gi
-  const backslash = /%2F/gi
-
-  return url.replace(colon, ':').replace(backslash, '/')
-}
-
-module.exports.easyIssues = async event => {
+module.exports.easyIssues = async (event) => {
   const body = parseBody(event.body)
-  const responseURL = parseURL(body.response_url)
+  const responseURL = decodeURIComponent(body.response_url)
+  console.log(`URL: ${body.response_url}`)
+  console.log(`decode: ${responseURL}`)
 
   const githubResponse = await fetch(GITHUB_URL)
   const githubJSON = await githubResponse.json()
 
-  const issuesArray = githubJSON.reduce(
-    (accumulator, issue) => {
-      const issueURL = 'https://' + issue.url.slice(12, 23) + issue.url.slice(29)
+  const issuesArray = githubJSON.reduce((accumulator, issue) => {
+    const issueURL = 'https://' + issue.url.slice(12, 23) + issue.url.slice(29) // convert API URL to Tasking Manager website URL
 
-      accumulator.push(
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `#${issue.number} - <${issueURL}|*${issue.title}*>`
+    accumulator.push(
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `#${issue.number} - <${issueURL}|*${issue.title}*>`,
+        },
+      },
+      {
+        type: 'section',
+        fields: issue.labels.map((label) => {
+          return {
+            type: 'plain_text',
+            text: label.name,
           }
-        },
-        {
-          type: "section",
-          fields: issue.labels.map(label => {
-            return {
-              type: "plain_text",
-              text: label.name
-            }
-          })
-        },
-        {
-          type: "divider"
-        }
-      )
-      return accumulator
-    }, []
-  )
-  
-  const BLOCK_COUNT = 45
+        }),
+      },
+      {
+        type: 'divider',
+      }
+    )
+    return accumulator
+  }, [])
+
   const slackMessages = groupIssuesIntoMessages(issuesArray, BLOCK_COUNT)
 
-  slackMessages.map(message => {
+  slackMessages.map((message) => {
     fetch(responseURL, {
       method: 'post',
       body: JSON.stringify(message),
-      headers: {'Content-Type': 'application/json'}
+      headers: { 'Content-Type': 'application/json' },
     })
   })
-  
+
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
     },
   }
 }
